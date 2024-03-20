30d9015899e0a6d428ed0e82809f7290c781b157
David Isinta <david.isinta@gmail.com>
2024-03-20 18:51:38 -0400

    post request w good naming conventions

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
+from random import randint
 
 app = FastAPI()
 
@@ -10,10 +11,17 @@ class Post(BaseModel):
     published:bool = True
     rating: Optional[int] = None
 
+my_posts = [{"title":"My first post","content":"", "id":1},
+            {"title":"My second post","content":"This is my second post","id":2}, {""}]
 
-@app.get("/")
+def find_posts(id):
+    for post in my_posts:
+        if post["id"] == id:
+            return post
+
+@app.get("/posts")
 async def root():
-    return {"message": "Hey thereeee, fullstack mern"}
+    return {"data": my_posts}
 
 
 @app.get("/hello/{name}")
@@ -24,6 +32,12 @@ async def say_hello(name: str):
 @app.post("/posts")
 def create_post(post: Post):
     # we want a title, and content, so we define a pydantic class that does that
-    print(post.rating)
-    print(post.dict())
-    return {"message": "gooda gooda"}
+    post_dict = post.dict()
+    post_dict["id"] = randint(1, 100000000)
+    my_posts.append(post_dict)
+    return {"post": post_dict}
+
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    return {"post": find_posts(id)}
