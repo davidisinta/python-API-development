9d185ac004e2dc6e52ceb41d4cb1bf609f06feb4
David Isinta <david.isinta@gmail.com>
2024-03-19 23:54:32 -0400

    first post request

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hey thereeee, fullstack mern"}
 
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+@app.post("/createposts")
+def create_post(payload: dict = Body(...)):
+    print(payload)
+    return {"message": "gooda gooda"}
